c1f01aa0f57ae238ff88cd3d8dca94be0262de3f
Arthur Scott <pr18ajs@leeds.ac.uk>
2025-04-10 16:24:05 +0100

    Add proper sorting to squares
    Squares now stay at 16 x 16 grid for any (valid) pixel width.

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 // Script to first generate 16 x 16 divs to go in container
 const gridDim = 16;
 const numberOfElements = gridDim * gridDim;
-const container = document.querySelector("div.container");
+const container = document.querySelector('div.container');
 
-for (let i = 0; i < numberOfElements; i++) {
-  const square = document.createElement("div");
-  square.classList.add("square");
-  container.appendChild(square);
+for (let i = 0; i < gridDim; i++) {
+    const column = document.createElement('div');
+    column.classList.add('column');
+
+    for (let j = 0; j < gridDim; j++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        column.appendChild(square);
+    }
+    container.appendChild(column);
 }
